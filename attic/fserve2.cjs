dcff8b1eeb6067f476b5c8fa2917b9eb172c1090
mark andrew <markandrew@mini.local>
2024-12-19 20:16:14 +0000

    almost there

--- attic/fserve2.cjs
+++ attic/fserve2.cjs
@@ -4,14 +4,21 @@ const path = require('path');
 
 const app = express();
 const PORT = 3000;
-const BASE_DIR = path.resolve('./content'); // Directory to browse
+const BASE_DIR = path.resolve('./content'); // Base directory to browse
 
 // Serve static files for the frontend
-// app.use(express.static('./public'));
-app.use(express.static(path.join(__dirname, 'content')));
+app.use(express.static('public'));
+
 // API to list files in a directory
 app.get('/api/files', (req, res) => {
-    fs.readdir(BASE_DIR, { withFileTypes: true }, (err, files) => {
+    const dir = req.query.dir || ''; // Get current directory from query
+    const targetDir = path.join(BASE_DIR, dir);
+
+    if (!targetDir.startsWith(BASE_DIR)) {
+        return res.status(403).json({ error: 'Access denied' });
+    }
+
+    fs.readdir(targetDir, { withFileTypes: true }, (err, files) => {
         if (err) {
             return res.status(500).json({ error: 'Unable to read directory' });
         }
@@ -27,7 +34,12 @@ app.get('/api/files', (req, res) => {
 // API to get the selected file
 app.get('/api/file/:name', (req, res) => {
     const fileName = req.params.name;
-    const filePath = path.join(BASE_DIR, fileName);
+    const dir = req.query.dir || ''; // Get current directory from query
+    const filePath = path.join(BASE_DIR, dir, fileName);
+
+    if (!filePath.startsWith(BASE_DIR)) {
+        return res.status(403).json({ error: 'Access denied' });
+    }
 
     if (!fs.existsSync(filePath) || fs.statSync(filePath).isDirectory()) {
         return res.status(400).json({ error: 'Invalid file selection' });
---
+++ podserver.cjs
@@ -0,0 +1,62 @@
+var express = require('express')
+var fs = require('fs')
+var path = require("path")
+
+var app = express()
+
+var port = 8014
+console = require("console"),
+    error = console.error
+//handlebars = require('express-handlebars'),
+    path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.get("/", (req, res, next) => {
+    let podPath = path.join(__dirname, "content")
+    let contents = fs.readdirSync(podPath)
+    let pcData = []
+    contents.forEach(file => {
+        console.log(file)
+        if (file !== ".DS_Store") {
+            pcData.push(file)
+        }
+    })
+    res.render("podcasts", { pods: pcData })
+})
+
+app.get("/pod/:id", (req, res, next) => {
+    let podName = req.params.id
+    let epPath = path.join(__dirname, "content", podName)
+    console.log("epPath ", epPath) 
+    let contents = fs.readdirSync(epPath)
+    let epData = []
+    contents.forEach(file => {
+        console.log(file)
+        if (file !== ".DS_Store") 
+            if (file.substring(file.length - 4) === ".mp3") {
+                fname = file.substring(0, file.length-4)
+                epData.push(fname)
+            }
+    })
+    res.render("episodes", { eps: epData, pod: podName })
+})
+
+
+
+
+
+
+app.get("/play/:pod/:ep", (req, res, next) => {
+    let pod = req.params.pod
+    let ep = req.params.ep
+    let epPath = path.join(__dirname, "content", pod, ep)
+    console.log("epPath", epPath)
+
+})
+
+
+app.listen(port, () =>
+    console.log(`Example app listening on port ${port}`)
+)
