097137023af8c9d366e19ac39f80072710d393da
mark andrew <markandrew@mini.local>
2025-03-02 08:56:47 +0000

    dockerusesname

---
+++ content/nhknraji/_config.md
@@ -0,0 +1,4 @@
+{
+    "feed": "https://www.nhk.or.jp/s-media/F532/podcast/list/v1/all.xml",
+    "nochunk": 1
+}
--- dockerrunrpm.sh
+++ dockerrunrpm.sh
@@ -1,1 +1,3 @@
-docker run -d -p 8014:8014 -v /volume1/data/languages/japanese/podcasts/content:/app/content transpod
+docker stop /transpod
+docker rm /transpod
+docker run --name transpod -d -p 8014:8014 -v /volume1/data/languages/japanese/podcasts/content:/app/content transpod
