d21d161f8fe275c131a52d1d7048a49402726a8a
mark andrew <markandrew@mini.local>
2025-03-06 19:03:02 +0000

    sort noriko on data and add published date to play page

--- podserver.cjs
+++ podserver.cjs
@@ -19,7 +19,7 @@ BADFILES = [".gitignore", ".DS_Store", "ReadMe.md"]
 console = require("console"),
     error = console.error
 //handlebars = require('express-handlebars'),
-    path = require('path');
+path = require('path');
 
 const exphbs = require('express-handlebars');
 const { getDefaultAutoSelectFamilyAttemptTimeout } = require('net')
@@ -38,46 +38,33 @@ app.use(express.json())
 
 app.get("/", (req, res, next) => {
     let podPath = path.join(__dirname, "content")
-    let contents = fs.readdirSync(podPath, {withFileTypes: true})
-    .filter(dirent => dirent.isDirectory())
-    .map(dirent => dirent.name)
+    let contents = fs.readdirSync(podPath, { withFileTypes: true })
+        .filter(dirent => dirent.isDirectory())
+        .map(dirent => dirent.name)
     let pcData = []
     contents.forEach(file => {
         // console.log(file)
-        if (!(BADFILES.includes(file))){
+        if (!(BADFILES.includes(file))) {
             let meta = readMetaPod(file)
-            podEntry = {name: file, ...meta}
+            podEntry = { name: file, ...meta }
             pcData.push(podEntry)
         }
     })
-    res.render("podcasts", { pods: pcData, layout: false})
+    res.render("podcasts", { pods: pcData, layout: false })
 })
 
-function compareEpisode (ep1, ep2) {
-    if (feedOrderDict) {
-        i1 = feedOrderDict[ep1.displayname]
-        i2 = feedOrderDict[ep2.displayname]
-        return i1 - i2
-    } else {
-    
+function comparePubdate(ep1, ep2) {
+
+}
+function compareEpisode(ep1, ep2) {
+
     // TBD include various sorting criteria here based on data in _config.md
     i1 = getIndex(ep1.displayname)
     i2 = getIndex(ep2.displayname)
     return i1.index.localeCompare(i2.index)
-    }
 
 }
 
-function makeFeedOrderDict(feed) {
-    let dict = {}
-    let index = 1
-    feed.forEach(ep => {
-        let cleanEp = ep.title.replace(/ /g, "_").replace(/\//g, "_") 
-        dict[cleanEp] = index;
-        index++
-    })
-    return dict
-}
 
 app.get("/pod/:id", (req, res, next) => {
     let podName = req.params.id
@@ -86,59 +73,49 @@ app.get("/pod/:id", (req, res, next) => {
     let contents = fs.readdirSync(epPath)
     let epData = []
     // find no of chunks for each file
-    let chunkdict = {}
-    let reg = /(^.*)chunk.*.mp3/
+    let sortonpubdate = false
 
-    let latestfeedPath = path.join(__dirname, "content", podName + ".latestfeed") 
-    if (fs.existsSync(latestfeedPath)) {
-        // console.log("latestfeed exists")
-        let latestFeed = JSON.parse(fs.readFileSync(latestfeedPath))
-        feedOrderDict = makeFeedOrderDict(latestFeed)
-    } else {
-        feedOrderDict = null
+    let latestfeedPath = path.join(__dirname, "content", podName + ".latestfeed")
+    if (fs.existsSync(latestfeedPath)) { // TODO change to use a sensible config value
+        sortonpubdate = true
     }
-  
-    contents.forEach(file => {
-        let matches = reg.exec(file);
-        if (matches) {
-            stem = matches[1]
-            if (!(Object.keys(chunkdict).includes(stem))) {
-                chunkdict[stem] = 0
-            }
-            chunkdict[stem]++
-        }
-    })
+
     contents.forEach(file => {
         // console.log(file)
         if (!(BADFILES.includes(file)))
             if (file.substring(file.length - 4) === ".mp3") {
                 // filter out all the chunks from the main list
-                if (!(file.includes("chunk"))) {
-                    fname = file.substring(0, file.length - 4)
-                    var chunklist = []
-                    if (Object.keys(chunkdict).includes(file)) {
-                        for (let i = 1; i <= chunkdict[file]; i++) {
-                            chunklist.push(i.toString().padStart(2, "0"))
-                        }
-                    }
-
-                    epData.push({ podName, displayname: fname, encoded: encodeURIComponent(fname), finished: !(isUnfinished(podName, fname)), chunks: chunklist })
+                fname = file.substring(0, file.length - 4)
+                // get the info file
+                let info = {}
+                let infopath = path.join(epPath, fname + ".info")
+                if (fs.existsSync(infopath)) {
+                    info = JSON.parse(fs.readFileSync(infopath, 'utf-8'))
                 }
+                epData.push({
+                    podName, displayname: fname, encoded: encodeURIComponent(fname),
+                    finished: !(isUnfinished(podName, fname)), info
+                })
+
             }
     })
 
     const meta = readMetaPod(podName)
     // if we only want unfinished
-    if(meta.show === "unfinished") {
+    if (meta.show === "unfinished") {
         epData = epData.filter((item) => isUnfinished(podName, item.displayname))
     }
     // throw out the finished episodes 
 
     // sort episodes
-    epData.sort(compareEpisode)
+    if (sortonpubdate) {
+        epData.sort((a, b) => comparePublishedParsed(b.info.published_parsed, a.info.published_parsed))
+    } else {
+        epData.sort(compareEpisode)
+    }
 
     // reverse order if dropdown set to latest
-  
+
     if (meta.order === "latest") {
         epData.reverse()
     }
@@ -151,13 +128,13 @@ app.get("/pod/:id", (req, res, next) => {
 
 function isUnfinished(podName, epName) {
     meta = readMetaEp(podName, epName)
-    return ! meta.finished 
+    return !meta.finished
 
 }
 
 function getNextep(ep) {
     if (!orderList)
-        return  
+        return
     let epData = orderList
     let found = false
     let nextep = ""
@@ -179,98 +156,109 @@ app.get("/play/:pod/:ep", (req, res, next) => {
     let epPath = path.join(__dirname, "content", pod, ep)
     mp3name = "/" + pod + "/" + encodeURIComponent(ep) + ".mp3"
     meta = readMetaEp(pod, ep)
- 
-//    transcriptfile = path.join(__dirname, "content", pod, ep + ".json")
-//    transcripttext = fs.readFileSync(transcriptfile)
-//  TODO apologize if no transcript is available
+
+    //    transcriptfile = path.join(__dirname, "content", pod, ep + ".json")
+    //    transcripttext = fs.readFileSync(transcriptfile)
+    //  TODO apologize if no transcript is available
     transcript = getTranscript(pod, ep)
     transcripttext = transcript.text
     transcriptsrc = transcript.src
-    nextep =  encodeURIComponent(getNextep(ep))
+    nextep = encodeURIComponent(getNextep(ep))
+    let infopath = epPath + ".info"
+    let info = {}
+    if (fs.existsSync(infopath)) {
+        info = JSON.parse(fs.readFileSync(infopath, 'utf-8'))
+    }   
 
-    res.render("playtrans", {pod, mp3file: mp3name, 
+    res.render("playtrans", {
+        pod, mp3file: mp3name,
         transcript: transcripttext,
-        source: transcriptsrc, meta, nextep, layout: false})
+        source: transcriptsrc, meta, nextep, 
+        info,
+        layout: false
+    })
 
 })
+
+function comparePublishedParsed(a, b) {
+    if (!a || !b) {
+        return 0
+    }   
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] !== b[i]) {
+            return a[i] - b[i];
+        }
+    }
+    return 0;
+}
 
 app.get("/recentPublish", (req, res) => {
-   
-    
-    function comparePublishedParsed(a, b) {
-        for (let i = 0; i < a.length; i++) {
-            if (a[i] !== b[i]) {
-                return a[i] - b[i];
-            }
-        }
-        return 0;
-    }
 
-          // Get all podcast info, sort on published_parsed field
-          let podPath = path.join(__dirname, "content")
-
-          let contents = fs.readdirSync(podPath, {withFileTypes: true})
-          .filter(dirent => dirent.isDirectory())
-          .map(dirent => dirent.name)
-          // for each podcast
-          let epList = []
-          contents.forEach(podName => {
-      
-              if (!(BADFILES.includes(podName))) {
-                  let ppath = path.join(podPath, podName)
-            
-                  let eps = fs.readdirSync(ppath, { withFileTypes: true })
-                              .filter(dirent => dirent.isFile() && dirent.name.endsWith('.info'))
-                              .map(dirent => dirent.name);
-                  eps.forEach(ep => {
-              
-                      let infopath = path.join(ppath, ep)
-           
-                      let info = JSON.parse(fs.readFileSync(infopath, 'utf-8'))
-           
-                      let barename = ep.slice(0, -5) 
-                      let epentry = {pod: podName, name: barename, encoded: encodeURIComponent(barename), info}
-                      epList.push(epentry)
-                  })
-              }
-          })
-          
-    
-    // Sort the data array based on the published_parsed field
-    epList.sort((a, b) => comparePublishedParsed(b.info.published_parsed, a.info.published_parsed))
-
-    // take the first 100
-    epList = epList.slice(0, 100)
-
-    res.render("recentPublish", {epList, layout: false})
-})
-
-app.get("/recentListen", (req, res) => {
-
-      // Get all podcast metadata, sort on timeLastOpened field, filter for unfinished
+    // Get all podcast info, sort on published_parsed field
     let podPath = path.join(__dirname, "content")
 
-    let contents = fs.readdirSync(podPath, {withFileTypes: true})
-    .filter(dirent => dirent.isDirectory())
-    .map(dirent => dirent.name)
+    let contents = fs.readdirSync(podPath, { withFileTypes: true })
+        .filter(dirent => dirent.isDirectory())
+        .map(dirent => dirent.name)
     // for each podcast
     let epList = []
     contents.forEach(podName => {
 
         if (!(BADFILES.includes(podName))) {
             let ppath = path.join(podPath, podName)
-      
+
             let eps = fs.readdirSync(ppath, { withFileTypes: true })
-                        .filter(dirent => dirent.isFile() && dirent.name.endsWith('.meta'))
-                        .map(dirent => dirent.name);
+                .filter(dirent => dirent.isFile() && dirent.name.endsWith('.info'))
+                .map(dirent => dirent.name);
             eps.forEach(ep => {
-        
+
+                let infopath = path.join(ppath, ep)
+
+                let info = JSON.parse(fs.readFileSync(infopath, 'utf-8'))
+
+                let barename = ep.slice(0, -5)
+                let epentry = { pod: podName, name: barename, encoded: encodeURIComponent(barename), info }
+                epList.push(epentry)
+            })
+        }
+    })
+
+
+    // Sort the data array based on the published_parsed field
+    epList.sort((a, b) => comparePublishedParsed(b.info.published_parsed, a.info.published_parsed))
+
+    // take the first 100
+    epList = epList.slice(0, 100)
+
+    res.render("recentPublish", { epList, layout: false })
+})
+
+app.get("/recentListen", (req, res) => {
+
+    // Get all podcast metadata, sort on timeLastOpened field, filter for unfinished
+    let podPath = path.join(__dirname, "content")
+
+    let contents = fs.readdirSync(podPath, { withFileTypes: true })
+        .filter(dirent => dirent.isDirectory())
+        .map(dirent => dirent.name)
+    // for each podcast
+    let epList = []
+    contents.forEach(podName => {
+
+        if (!(BADFILES.includes(podName))) {
+            let ppath = path.join(podPath, podName)
+
+            let eps = fs.readdirSync(ppath, { withFileTypes: true })
+                .filter(dirent => dirent.isFile() && dirent.name.endsWith('.meta'))
+                .map(dirent => dirent.name);
+            eps.forEach(ep => {
+
                 let metapath = path.join(ppath, ep)
-     
+
                 let meta = JSON.parse(fs.readFileSync(metapath, 'utf-8'))
-     
-                let barename = ep.slice(0, -5) 
-                let epentry = {pod: podName, name: barename, encoded: encodeURIComponent(barename), meta}
+
+                let barename = ep.slice(0, -5)
+                let epentry = { pod: podName, name: barename, encoded: encodeURIComponent(barename), meta }
                 epList.push(epentry)
             })
         }
@@ -281,10 +269,10 @@ app.get("/recentListen", (req, res) => {
     // take the first 100
     epList = epList.slice(0, 100)
 
-    res.render("recentListen", {epList, layout: false})
+    res.render("recentListen", { epList, layout: false })
 })
 
-function readMetaEp(pod, ep)  {
+function readMetaEp(pod, ep) {
     const metaPath = path.join(__dirname, "content", pod, ep + ".meta")
     if (fs.existsSync(metaPath)) {
         try {
@@ -293,7 +281,7 @@ function readMetaEp(pod, ep)  {
             console.error(`Error reading ${metaPath}:`, error);
         }
     }
-    return { finished: false, timeLastOpened: 0, timeInPod: 0}; // Default values
+    return { finished: false, timeLastOpened: 0, timeInPod: 0 }; // Default values
 }
 
 
@@ -309,8 +297,8 @@ function readMetaPod(folderName) {
     return { order: "latest", show: "all" }; // Default values
 }
 
-function writeMetaEp(metaPath, finished, timeLastOpened, timeInPod){
-    const metaData = {finished, timeLastOpened, timeInPod}
+function writeMetaEp(metaPath, finished, timeLastOpened, timeInPod) {
+    const metaData = { finished, timeLastOpened, timeInPod }
     fs.writeFileSync(metaPath, JSON.stringify(metaData, null, 4), 'utf-8')
     console.log(`Updated meta ep file: ${metaPath}`);
 }
@@ -319,7 +307,7 @@ function writeMetaEp(metaPath, finished, timeLastOpened, timeInPod){
 function writeMetaPod(folderName, order, show) {
     const metaPath = path.join(__dirname, "content", `${folderName}.meta`);
     const metaData = { order, show };
-    
+
     try {
         fs.writeFileSync(metaPath, JSON.stringify(metaData, null, 4), 'utf-8');
         console.log(`Updated meta pod file: ${metaPath}`);
@@ -336,21 +324,21 @@ app.post('/update-meta-ep', (req, res) => {
     // TBD cut off mp3 and change to meta
     // TBD maybe some URL ding needed here
     const deconame = decodeURIComponent(name)
-    const podcastpath = deconame.slice(0,-4)
+    const podcastpath = deconame.slice(0, -4)
     const metapath = path.join(__dirname, "content", podcastpath + ".meta")
     try {
-         writeMetaEp(metapath, finished, timeLastOpened, timeInPod)
+        writeMetaEp(metapath, finished, timeLastOpened, timeInPod)
 
         res.json({ success: true });
     } catch (error) {
         console.error(`Error writing ${metapath}`, error)
-        res.status(400).json({  success: false, message: "Invalid Episode"});
+        res.status(400).json({ success: false, message: "Invalid Episode" });
     }
 
 });
 
 app.post('/update-meta-pod', (req, res) => {
- 
+
     const { name, order, show } = req.body;
     const folderPath = path.join(__dirname, "content", name);
 
@@ -374,7 +362,7 @@ function findFileInDirectory(directory, searchString) {
         const matchingFiles = files.filter(file => file.includes(searchString));
 
         if (matchingFiles.length > 0) {
-         
+
             return matchingFiles
         } else {
             console.error("No matching files found.");
@@ -389,74 +377,74 @@ function getIndex(title) {
     const match = title.match(/#(\d+)/)
     if (match) {
         const paddedNumber = match[1].padStart(4, '0');
-        return {match: true, index: paddedNumber}
+        return { match: true, index: paddedNumber }
     }
     else
-       return {match: false, index: title}
-} 
+        return { match: false, index: title }
+}
 
 const getTranscript = (pod, ep) => {
     // 1 Is there an html transcript
     //    1.1 Derive canonical index
     // const match = ep.match(/^#(\d+)/)
-    const {match, index} = getIndex(ep)
+    const { match, index } = getIndex(ep)
     paddedNumber = ""
     transfolder = ""
     let source = "whisper"
     foundtranscript = false
     transcripttext = ""
 
-    if  (match) {
+    if (match) {
 
-    //    1.2 Look for file containing canonical index in transcripts folder
+        //    1.2 Look for file containing canonical index in transcripts folder
 
-            transfolder = path.join(__dirname, "content", pod, "transcripts")
-            const res = findFileInDirectory(transfolder, index)
-            if (res.length == 1) {
-                foundtranscript = res[0]
-            } 
-    
-    // 2 If so convert to json and use that
+        transfolder = path.join(__dirname, "content", pod, "transcripts")
+        const res = findFileInDirectory(transfolder, index)
+        if (res.length == 1) {
+            foundtranscript = res[0]
+        }
+
+        // 2 If so convert to json and use that
     }
     if (foundtranscript) {
         source = "patreon"
         transcripttext = transhtml(path.join(transfolder, foundtranscript))
     } else {
-    // 3 Else use the whisper thing from the json file
+        // 3 Else use the whisper thing from the json file
         transcriptfile = path.join(__dirname, "content", pod, ep + ".json")
         try {
-        transcripttext = fs.readFileSync(transcriptfile)
-        } catch(error) {
+            transcripttext = fs.readFileSync(transcriptfile)
+        } catch (error) {
             console.log("no transcript found, defaulting to polite apology")
             transcripttext = '[{ "start": 0.0, "end": 10000.0, "text": "申し訳ありませんが、このエピソードのトランスクリプトはまだ利用できません。"}]'
         }
     }
 
-    return {src: source, text: transcripttext}
-} 
+    return { src: source, text: transcripttext }
+}
 
-const transhtml = (fname) => { 
+const transhtml = (fname) => {
     const html = fs.readFileSync(fname, 'utf8');
 
- // Parse the HTML
+    // Parse the HTML
     const dom = new JSDOM(html);
     const document = dom.window.document;
-    
+
     // Extract subtitles
     const subtitles = [];
     document.querySelectorAll('div').forEach(div => {
         const timestamp = div.querySelector('.timestamp');
         const textElement = div.querySelector('.subtitle-text');
-        
+
         if (timestamp && textElement) {
             const start = parseTimestamp(timestamp.getAttribute('data-subbegin'));
             const end = parseTimestamp(timestamp.getAttribute('data-subend'));
             const text = textElement.textContent.trim();
-            
+
             subtitles.push({ start, end, text });
         }
     });
-    
+
     // Save as JSON
     return JSON.stringify(subtitles, null, 4)
 
